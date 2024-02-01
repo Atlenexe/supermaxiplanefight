b79e90af1694ff8a6562517fc43e65c1dba7dd26
Atlenexe <hugo.mandou@gmail.com>
2024-02-01 11:35:59 +0100

    Tests canvas

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log("Hello World!");
+
+window.onload = function () {
+    let canvas = document.getElementById("gameView");
+
+    const canvasHeight = 600;
+    const canvasWidth = 600;
+
+    canvas.height = canvasHeight;
+    canvas.width = canvasWidth;
+
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+
+        const gameViewHeight = canvas.height;
+        const gameViewWidth = canvas.width;
+
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, gameViewHeight, gameViewWidth);
+
+        ctx.fillStyle = "red";
+        ctx.fillRect(0, 0, 100, 100);
+    } else {
+    }
+}
